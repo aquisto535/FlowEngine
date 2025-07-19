868f37ce3753060377e0ab75e0e0ff22a8cef609
Jung Tae Min <71391013+aquisto535@users.noreply.github.com>
2025-07-19 15:53:22 +0900

    Project Setting6 Complete!

---
+++ NexusCore.Tests.Common/CommonTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NexusCoreTestsCommon
+{
+	TEST_CLASS(NexusCoreTestsCommon)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ NexusCore.Tests.Networking/NexusCore.Tests.Networking.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NexusCoreTestsNetworking
+{
+	TEST_CLASS(NexusCoreTestsNetworking)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
